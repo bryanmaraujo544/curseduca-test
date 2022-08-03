965f4781a8821554f3fd75ed56eebb5d7559a2ec
Bryan Martins <bryanmaraujo544@gmail.com>
2022-08-02 21:34:11 -0300

    chore: fixing api url in production

--- src/services/serverApi.ts
+++ src/services/serverApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : 'http://localhost:8080';
+    : 'https://curseduca-api.herokuapp.com/';
 
 export const serverApi = axios.create({
   baseURL: apiUrl,
